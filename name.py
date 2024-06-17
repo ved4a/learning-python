550e96d659571e26f1659942bdcbff8cb1c443ba
Vedaa Anand <work.vedaa@gmail.com>
2024-06-17 14:11:24 +0530

    feat: Use methods on strings

---
+++ name.py
@@ -0,0 +1,8 @@
+name = 'monty python'
+print(name.title()) # this is a method that will return Monty Python
+
+scientist = 'Isaac Newton'
+print(scientist.upper()) # method -> ISAAC NEWTON
+print(scientist.lower()) # method -> isaac newton
+
+# A "method" is an action that Python can perform on a piece of data. The dot after the variable name indicates to Python as to which piece of data the method is being called on- in this case "scientist" and "name". The parentheses are used to pass additional information/instructions to the method (in this case, it is unnecessary, but exists to ensure syntactic consistency).
