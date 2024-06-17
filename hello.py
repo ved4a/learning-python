8c86e93b9e8e5e5c3dcdffe5d7c4adfd8b28903c
Vedaa Anand <work.vedaa@gmail.com>
2024-06-17 14:05:16 +0530

    feat: Finish 'Variables' section

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print('Hello, world!')
+# print('Hello, world!')
+message = 'Hello Python world!' # variables is message, value is "Hello Python world!"
+print(message) # if incorrect var name (eg: mesage), "NameError" will be raised
+
+message = "Hello Crash Course Python!"
+print(message)
+
+# 2-1
+sayHello = "Hi, nice to meet you!"
+print(sayHello)
+ #2-2
+sayHello = "Hi, haven't met you before!"
+print(sayHello)
+
+quote_1 = "The language 'Python' is actually named after Monty Python, not the snake."
+print(quote_1)
+
+quote_2 = "So I told my friend, 'Ricky, when I catch you Ricky', yet there was no fear in his eyes."
+print(quote_2)
