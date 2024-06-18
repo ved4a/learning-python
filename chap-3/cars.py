12534f2a291047ffa98efff803b633678a7f8436
Vedaa Anand <work.vedaa@gmail.com>
2024-06-18 11:18:55 +0530

    Organize a list

---
+++ chap-3/cars.py
@@ -0,0 +1,19 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+# to permanently sort the cars alphabetically:
+# cars.sort()
+# print(cars)
+
+# to permanently sort the cars reverse-alphabetically:
+# cars.sort(reverse= True)
+# print(cars)
+
+# to temporarily sort cars:
+# print(sorted(cars))
+
+# reversing the order of a list
+cars.reverse()
+print(cars)
+
+# length of a list
+print(len(cars))
