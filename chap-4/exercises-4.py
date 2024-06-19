61c17299e39a6afcec9b680f21203ed1b7b26d32
Vedaa Anand <work.vedaa@gmail.com>
2024-06-19 10:11:09 +0530

    Finish exercises 4-1 and 4-2

---
+++ chap-4/exercises-4.py
@@ -0,0 +1,11 @@
+# 4-1
+pizzas = ['margherita', 'pepperoni', 'veggie', 'double cheese']
+for pizza in pizzas:
+    print(f"I like {pizza.title()} pizza.")
+print("If you couldn't tell, I really like pizza!")
+
+# 4-2
+pets = ['dog', 'cat', 'bird', 'fish']
+for pet in pets:
+    print(f"A {pet} would make a good pet.")
+print('Honestly, anything would make a good pet!')
