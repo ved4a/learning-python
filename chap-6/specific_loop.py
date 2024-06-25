b3b99ff54b2ad8d55f0f9475527aae22f2ea502f
Vedaa Anand <work.vedaa@gmail.com>
2024-06-25 15:15:11 +0530

    Loop through keys specifically

---
+++ chap-6/specific_loop.py
@@ -0,0 +1,28 @@
+# looping through keys
+languages_spoken = {
+    'hitori': 'japanese',
+    'nikita': 'russian',
+    'meg': 'english',
+    'kannan': 'tamil'
+}
+
+for name in languages_spoken.keys():
+    print(name.title())
+
+# however, for dictionaries, the key is actually the default value, so:
+for name in languages_spoken:
+    print(name.title()) # this gives the same output
+
+# using lists and dictionaries in tandem:
+friends = ['kannan', 'nikita']
+
+for name in languages_spoken:
+    print(f"Hi, {name.title()}.")
+
+    if name in friends:
+        language = languages_spoken[name].title()
+        print(f"\t{name.title()}, I see you speak {language}!")
+
+# can also use keys method to check if key exists in dict or no
+if 'krish' not in languages_spoken.keys():
+    print('Krish, please take the poll!')
