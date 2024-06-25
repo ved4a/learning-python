0f48aa8ea92ca3df86a855901e715a0ca131cdb4
Vedaa Anand <work.vedaa@gmail.com>
2024-06-25 15:46:30 +0530

    Dictionary in list

---
+++ chap-6/nested.py
@@ -0,0 +1,36 @@
+# a list of dictionaries
+# alien_0 = {'color': 'green', 'points': 5}
+# alien_1 = {'color': 'yellow', 'points': 10}
+# alien_2 = {'color': 'red', 'points': 15}
+
+# aliens = [alien_0, alien_1, alien_2]
+
+# for alien in aliens:
+#     print(alien)
+
+# automatically generating aliens is more realistic:
+# create empty list
+aliens = []
+
+# make 30 aliens
+for alien in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
+# show the first 5 aliens
+for alien in aliens[:5]:
+    print(alien)
+print('...')
+
+# total number of aliens created:
+print(f'The total number of aliens are: {len(aliens)}')
