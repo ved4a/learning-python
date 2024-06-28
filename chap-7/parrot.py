da31addd729d46f9e70eda233e777bff475f3921
Vedaa Anand <work.vedaa@gmail.com>
2024-06-28 10:10:59 +0530

    Introduction to input()

---
+++ chap-7/parrot.py
@@ -0,0 +1,5 @@
+# the input function pauses the program until user input is received
+# that input is then stored into a variable which can be used, eg:
+
+message = input("Tell me something, and I'll repeat it back to you: ")
+print(message)
